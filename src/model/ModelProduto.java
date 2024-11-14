06563879ab27d5c8bf46e683f9639a3d17115f69
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-11-14 11:40:43 -0300

    Feat(Model): add model product

---
+++ src/model/ModelProduto.java
@@ -0,0 +1,32 @@
+package model;
+
+public abstract class ModelProduto {
+
+	private String nome;
+	private double preco;
+
+	public ModelProduto(String nome, double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public abstract void detalhes();
+
+}
